757c9296a4e30c3683b0fdf99d5377adfc3052fc
Giacomo Bergami <bergamigiacomo@gmail.com>
2024-03-18 20:38:29 +0000

    Configuration files and setting

--- start_services.sh
+++ start_services.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 pushd submodules/news-crawler
+chmod +x main.py
 pip3 install -r requirements.txt
 popd
 pushd submodules/stanford_dg_server
-mvn compile exec:java -Dexec.mainClass="uk.ncl.giacomobergami.Main" & # Starting the java service
+mvn compile exec:java -Dexec.mainClass="uk.ncl.giacomobergami.Main" -Dexec.workingdir="../../" & # Starting the java service
 popd
+python3 submodules/news-crawler/main.py                               #
