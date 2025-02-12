c27ca6b9eb4766654dc3970ff1b11068bc3493ee
Franco-21-S <F.Saez2@newcastle.ac.uk>
2025-02-12 17:52:31 +0000

    added arg parsing

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from src.ontology_generator import generate
-
+import argparse
 
 if __name__ == "__main__":
-    generate()
+    parser = argparse.ArgumentParser()
+
+    # Adding required arguments for paths
+    parser.add_argument("-c", "--conceptnet", type=str, required=True, help="Path to coneptnet csv file")
+    parser.add_argument("-j", "--wiktionary", type=str, required=True, help="Path to wiktionary json file")
+
+    args = parser.parse_args()
+    generate(args.conceptnet, args.wiktionary)
