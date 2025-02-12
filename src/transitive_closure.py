050f0e5444201d02fcb074c9d25ae28ccd650ecc
Franco-21-S <F.Saez2@newcastle.ac.uk>
2025-02-12 17:26:58 +0000

    floyd warsh and cluster building

--- src/transitive_closure.py
+++ src/transitive_closure.py
@@ -1,24 +1,60 @@
 from sqlitedict import SqliteDict
 
-def build_clusters():
-    """
-    nodes = adj_db.getNodes()
+"""
+  nodes = adj_db.getNodes()
 
-    initializing clusters for each node, that just includes said node
-    for n in nodes:
-        id = new_id()
-        cluster_members_db[id] = [n]
-        cluster_mapping_db[n] = id
+  initializing clusters for each node, that just includes said node
+  for n in nodes:
+      id = new_id()
+      cluster_members_db[id] = [n]
+      cluster_mapping_db[n] = id
 
-    it's gonna be like:
-    for i in nodes:
-    for j in nodes:
-    for k in nodes:
-    if i == j and j == k:
-    do cluster thing
-    """
+  it's gonna be like:
+  for i in nodes:
+  for j in nodes:
+  for k in nodes:
+  if i == j and j == k:
+  do cluster thing
+  """
+def build_closures():
     adjacency_db = SqliteDict('adjacency_list.db')
+
+    for i in adjacency_db.keys():
+        for j in adjacency_db.keys():
+
+            if j not in adjacency_db[i]: continue
+
+            # at this point, i -> j
+
+            for k in adjacency_db.keys():
+
+                if k not in adjacency_db[j]: continue
+
+                # at this point, j -> k, hence i -> j -> k
+
+                if k in adjacency_db[i]: continue ## not completely sure if this is necessary, it's to guard against duplicates
+                adjacency_list = adjacency_db[i]
+                adjacency_list.append(k)
+                adjacency_db[i] = adjacency_list # i -> k
+                # a later iteration will make this i <-> k (bijective)
+
+    adjacency_db.commit()
+    adjacency_db.close()
+
+
+def build_clusters():
     cluster_db = SqliteDict('clusters.db')
+    adjacency_db = SqliteDict('adjacency_list.db')
 
-    for
+    for key_node, adjacency_list in adjacency_db.items():
+        if cluster_db.get(key_node): continue
 
+        adjacency_list.append(key_node)
+        cluster = sorted(adjacency_list)
+
+        for node in cluster:
+            cluster_db[node] = cluster
+
+    cluster_db.commit()
+    cluster_db.close()
+    adjacency_db.close()
