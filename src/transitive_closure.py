d48ac943d782b8050f3f1351117d1a8ebe934427
Franco-21-S <F.Saez2@newcastle.ac.uk>
2025-02-14 05:30:02 +0000

    fixed floyd warshall self loop

--- src/transitive_closure.py
+++ src/transitive_closure.py
@@ -1,10 +1,12 @@
 from sqlitedict import SqliteDict
 
 
-def build_closures():
-    adjacency_db = SqliteDict("adjacency_list.db")
-
+def floyd_warshall(adjacency_db):
+    count = 0
     for i in adjacency_db.keys():
+        count += 1
+        print(count)
+        if count % 1000 == 0: print(count)
         for j in adjacency_db.keys():
 
             if j not in adjacency_db[i]: continue
@@ -14,14 +16,23 @@ def build_closures():
             for k in adjacency_db.keys():
 
                 if k not in adjacency_db[j]: continue
+                if i == k: continue # this prevents self loops (build_clusters accomplishes this)
 
                 # at this point, j -> k, hence i -> j -> k
 
-                if k in adjacency_db[i]: continue ## not completely sure if this is necessary, it's to guard against duplicates
+                if k in adjacency_db[i]: continue  ## not completely sure if this is necessary, it's to guard against duplicates
                 adjacency_list = adjacency_db[i]
                 adjacency_list.append(k)
-                adjacency_db[i] = adjacency_list # i -> k
+                adjacency_db[i] = adjacency_list  # i -> k
                 # a later iteration will make this i <-> k (bijective)
+    return adjacency_db
+
+
+def build_closures():
+    adjacency_db = SqliteDict("adjacency_list.db")
+
+    print("expanding adjacencny list with transitive closure")
+    floyd_warshall(adjacency_db)
 
     adjacency_db.commit()
     adjacency_db.close()
@@ -31,8 +42,13 @@ def build_clusters():
     cluster_db = SqliteDict("clusters.db")
     adjacency_db = SqliteDict("adjacency_list.db")
 
+    print("building clusters")
+    count = 0
     for key_node, adjacency_list in adjacency_db.items():
         if cluster_db.get(key_node): continue
+
+        count += 1
+        if count % 1000 == 0: print(count)
 
         adjacency_list.append(key_node)
         cluster = sorted(adjacency_list)
