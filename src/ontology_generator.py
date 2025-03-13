6ce742ec398dc62ef9b2675291aa3602babe1ff4
Franco-21-S <F.Saez2@newcastle.ac.uk>
2025-03-13 05:09:13 +0000

    doing wiktionary json processing line by line

--- src/ontology_generator.py
+++ src/ontology_generator.py
@@ -4,27 +4,49 @@ import transitive_closure
 import parse_conceptnet_file
 import wiktionary_json_extract
 import json
+import csv
 from config import config
 from sqlitedict import SqliteDict
-import csv
 
 
-def generate(conceptnet_path, wiktionary_path, test_limit=-1):
+def from_db(db_name):
+    db = SqliteDict(db_name)
+    dict = {}
+    for key, value in db.items():
+        dict[key] = value
+    db.close()
+    return dict
+
+def to_db(db_name, dict):
+    db = SqliteDict(db_name)
+    for key, value in dict.items():
+        db[key] = value
+    db.commit()
+    db.close()
+
+def generate(conceptnet_path, wiktionary_path, load_from_db=False, test_limit=-1):
 
     print("hi")
-    adjacency, clusters = {}, {}
+    adjacency = from_db("adjacency_list.db") if load_from_db else {}
+    clusters = from_db("clusters.db") if load_from_db else {}
     # adjacency, clusters = SqliteDict("adjacency_list.db"), SqliteDict("clusters.db")
 
-    adjacency_list.process_conceptnet_csv(conceptnet_path, adjacency)
+    if not load_from_db:
+        adjacency_list.process_conceptnet_csv(conceptnet_path, adjacency)
 
-    transitive_closure.build_closures(adjacency)
-    transitive_closure.build_clusters(adjacency, clusters)
+        transitive_closure.build_closures(adjacency)
+        transitive_closure.build_clusters(adjacency, clusters)
+
+        to_db("adjacency_list.db", adjacency)
+        to_db("clusters.db", clusters)
+
 
     with open(config["result_file"], "w", encoding="utf-8") as tsv:
         wr = csv.writer(tsv, delimiter="\t")
         wr.writerow(["source", "relation", "target"])
 
         print("making result file")
+        print("doing conceptnet")
         count = 0
         for triplet in parse_conceptnet_file.get_triplets(conceptnet_path, lang="en"):
             (source, edge_label, target) = triplet
@@ -32,24 +54,37 @@ def generate(conceptnet_path, wiktionary_path, test_limit=-1):
             wr.writerow((transitive_closure.get_node(clusters, source), edge_mapping.get_edge(edge_label),
                          transitive_closure.get_node(clusters, target)))
             count += 1
-            if count % 1000 == 0: print(count)
+            if count % 100000 == 0: print(count)
             if count == test_limit: break
 
-        with open(wiktionary_path, 'r', encoding='utf-8') as file:
-            wik_json = json.load(file)
-            for triplet in wiktionary_json_extract.extract_information(wik_json, language_code="en"):
-                (source, edge_label, target) = triplet
-                source = transitive_closure.get_node(clusters, source.split("#")[0])
-                target = transitive_closure.get_node(clusters, target.split("#")[0])
+        print("doing wiktionary")
+        count = 0
+        with open(wiktionary_path, 'r', encoding="utf-8") as file:
+            # wik_json = json.load(file)
+            for line in file:
+                line = line.strip()
+                if not line: continue
 
-                wr.writerow((source, edge_mapping.get_edge(edge_label), target))
+                entry = json.loads(line)
 
-    if type(adjacency) == SqliteDict:
-        adjacency.close()
+                count += 1
+                if count % 100000 == 0: print(count)
 
-    if type(clusters) == SqliteDict:
-        clusters.close()
+                generator = wiktionary_json_extract.extract_information(entry, language_code="en")
+                if not generator: continue
 
+                for triplet in generator:
+                    (source, edge_label, target) = triplet
+                    source = transitive_closure.get_node(clusters, source.split("#")[0])
+                    target = transitive_closure.get_node(clusters, target.split("#")[0])
+
+                    wr.writerow((source, edge_mapping.get_edge(edge_label), target))
+
+    # if type(adjacency) == SqliteDict:
+    #     adjacency.close()
+    #
+    # if type(clusters) == SqliteDict:
+    #     clusters.close()
 
 
 
