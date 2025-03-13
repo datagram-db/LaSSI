cec64260c07c7c17b5a5414cf620b13e69eca105
Franco-21-S <F.Saez2@newcastle.ac.uk>
2025-03-13 01:37:34 +0000

    floyd warshall optimized and new algorithm added

--- src/ontology_generator.py
+++ src/ontology_generator.py
@@ -2,12 +2,15 @@ import edge_mapping
 import adjacency_list
 import transitive_closure
 import parse_conceptnet_file
+import wiktionary_json_extract
+import json
 from config import config
 import csv
 
 
 def generate(conceptnet_path, wiktionary_path, test_limit: -1):
 
+    print("hi")
     adjacency_list.process_conceptnet_csv(conceptnet_path)
 
     transitive_closure.build_closures()
@@ -17,13 +20,27 @@ def generate(conceptnet_path, wiktionary_path, test_limit: -1):
         wr = csv.writer(tsv, delimiter="\t")
         wr.writerow(["source", "relation", "target"])
 
+        print("making result file")
         count = 0
         for triplet in parse_conceptnet_file.get_triplets(conceptnet_path, lang="en"):
-            (source, relation, target) = triplet
-            wr.writerow((transitive_closure.get_node(source), edge_mapping.get_edge(relation), transitive_closure.get_node(target)))
+            (source, edge_label, target) = triplet
+            #wr.writerow((transitive_closure.get_node(source), edge_mapping.get_edge(relation), transitive_closure.get_node(target)))
+            wr.writerow((transitive_closure.get_node(source), edge_mapping.get_edge(edge_label),
+                         transitive_closure.get_node(target)))
             count += 1
+            if count % 1000 == 0: print(count)
             if count == test_limit: break
-        ### another loop but for wiktionary:
+
+        wik_json = json.load(wiktionary_path)
+        for triplet in wiktionary_json_extract.extract_information(wik_json, language_code="en"):
+            (source, edge_label, target) = triplet
+            source = transitive_closure.get_node(source.split("#")[0])
+            target = transitive_closure.get_node(target.split("#")[0])
+
+            wr.writerow((source, edge_mapping.get_edge(edge_label),target))
+
+
+
 
 
 
