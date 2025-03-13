bf61b1b2ce23a1e1ee06a1cb43e99866d24fe211
Franco-21-S <F.Saez2@newcastle.ac.uk>
2025-03-13 01:25:28 +0000

    some changes

--- tests/transitive_test.py
+++ tests/transitive_test.py
@@ -1,12 +1,19 @@
 import pytest
-from src.transitive_closure import floyd_warshall
+import time
+from src.transitive_closure import floyd_warshall, DSU
 
 def sort_adjacency_list(adj):
     for k, v in adj.items():
         adj[k] = sorted(v)
 
-def test_small_graph1():
-    adjacency_list = {
+def timer(callback):
+    start_time = time.perf_counter()
+    callback()
+    print(f"Test took {(time.perf_counter() - start_time) * 1000:.3f} ms")
+
+
+def test_floyd_1():
+    adjacency_list = {   ## just to see if it works with directed graph
         "A": ["B"],
         "B": ["C"],
         "C": []
@@ -17,12 +24,15 @@ def test_small_graph1():
         "C": []
     }
 
-    result = floyd_warshall(adjacency_list)
-    sort_adjacency_list(result)
-    assert result == expected_result
+    def f():
+        floyd_warshall(adjacency_list)
+
+    timer(f)
+    sort_adjacency_list(adjacency_list)
+    assert adjacency_list == expected_result
 
 
-def test_small_graph2():
+def test_floyd_2():
     adjacency_list = {
         "A": ["B", "C"],
         "B": ["A"],
@@ -34,6 +44,68 @@ def test_small_graph2():
         "C": ["A", "B"]
     }
 
-    result = floyd_warshall(adjacency_list)
-    sort_adjacency_list(result)
-    assert result == expected_result
+    def f():
+        floyd_warshall(adjacency_list)
+
+    timer(f)
+    sort_adjacency_list(adjacency_list)
+    assert adjacency_list == expected_result
+
+def test_DSU_1():
+    adjacency_list = {
+        "A": ["B", "C"],
+        "B": ["A"],
+        "C": ["A"]
+    }
+    expected_result = {
+        "A": ["B", "C"],
+        "B": ["A", "C"],
+        "C": ["A", "B"]
+    }
+
+    def f():
+        DSU(adjacency_list)
+
+    timer(f)
+    sort_adjacency_list(adjacency_list)
+    assert adjacency_list == expected_result
+
+
+def test_DSU_2():
+    adjacency_list = {
+        "A": ["B"],
+        "B": ["A", "C"],
+        "C": ["B", "D"],
+        "D": ["C"],
+        "E": ["F"],
+        "F": ["E", "G"],
+        "G": ["F", "H"],
+        "H": ["G", "I"],
+        "I": ["H"],
+        "J": ["K"],
+        "K": ["J", "L"],
+        "L": ["K"]
+    }
+
+    expected_result = {
+        "A": ["B", "C", "D"],
+        "B": ["A", "C", "D"],
+        "C": ["A", "B", "D"],
+        "D": ["A", "B", "C"],
+        "E": ["F", "G", "H", "I"],
+        "F": ["E", "G", "H", "I"],
+        "G": ["E", "F", "H", "I"],
+        "H": ["E", "F", "G", "I"],
+        "I": ["E", "F", "G", "H"],
+        "J": ["K", "L"],
+        "K": ["J", "L"],
+        "L": ["J", "K"]
+    }
+
+    def f():
+        DSU(adjacency_list)
+
+    timer(f)
+    sort_adjacency_list(adjacency_list)
+    assert adjacency_list == expected_result
+
